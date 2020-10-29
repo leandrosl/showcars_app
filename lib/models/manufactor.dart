45e9f192d44ee72604ba668d62f8b1e53b146944
Leandro Souza <sl.leandro09@gmail.com>
2020-10-29 12:32:18 -0300

    Convertendo os valores inteiros do json para String na classe Manufactor e adição de parametro id

--- lib/models/manufactor.dart
+++ lib/models/manufactor.dart
@@ -1,12 +1,14 @@
 class Manufactor {
+  String id;
   String name;
   String country;
   String logoUrl;
 
-  Manufactor({this.name, this.country, this.logoUrl});
+  Manufactor({this.id, this.name, this.country, this.logoUrl});
 
   factory Manufactor.fromJson(Map<String, dynamic> json) {
     return Manufactor(
+      id: json["id"].toString(),
       name: json['name'],
       country: json['country'],
       logoUrl: json['logo'],
