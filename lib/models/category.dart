2019394f6c49262a2dd364e5a649fe0c30d2c58d
Leandro Souza <sl.leandro09@gmail.com>
2020-10-31 18:17:46 -0300

    Propriedade id incluída na classe Category

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,11 +1,13 @@
 class Category {
+  String id;
   String name;
   String imageUrl;
 
-  Category({this.name, this.imageUrl});
+  Category({this.id, this.name, this.imageUrl});
 
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
+      id: json['id'].toString(),
       name: json['name'],
       imageUrl: json['category_image'],
     );
