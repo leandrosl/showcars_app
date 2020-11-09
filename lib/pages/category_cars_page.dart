945d86d0fea1c537679f82b79e2c60a3f17085be
Leandro Souza <sl.leandro09@gmail.com>
2020-11-09 16:53:32 -0300

    Inclusão de lógica para exibir carros pela categoria selecionada

--- lib/pages/category_cars_page.dart
+++ lib/pages/category_cars_page.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 import 'package:showcars_app/models/category.dart';
+import 'package:showcars_app/repositories/car_repository.dart';
 
 import 'package:showcars_app/repositories/category_repository.dart';
 
+import 'cars_list_page.dart';
+
 class CategoryCarsPage extends StatelessWidget {
-  CategoryRepository _repository;
-  
   @override
   Widget build(BuildContext context) {
-    _repository = CategoryRepository();
+    CategoryRepository _categoryRepository = CategoryRepository();
+    CarRepository _carRepository = CarRepository();
 
     return Scaffold(
       body: Container(
         child: FutureBuilder(
-          future: _repository.getCategories(),
+          future: _categoryRepository.getCategories(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (snapshot.connectionState != ConnectionState.done) {
               return Center(
@@ -35,51 +37,76 @@ class CategoryCarsPage extends StatelessWidget {
               scrollDirection: Axis.vertical,
               itemCount: snapshot.data.length,
               itemBuilder: (BuildContext context, int index) {
-                return _listItemCard(snapshot.data[index]);
-              }
+                Category _category = snapshot.data[index];
+
+                return CategoryListItem(
+                  category: _category,
+                  onTap: () {
+                    _carRepository.getCarsByCategory(_category.id).then((cars) {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => CarsListPage(
+                          cars: cars,
+                          pageTitle: 'Carros - ${_category.name}',
+                        ),
+                      ));
+                    });
+                  },
+                );
+              },
             );
-          }
+          },
         ),
       ),
     );
   }
+}
 
-  Widget _listItemCard(Category category) {
-    return Card(
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: 120,
-          ),
-          Positioned.fill(
-              child: FadeInImage(
-                fit: BoxFit.cover,
-                image: NetworkImage('http://192.168.0.210:5200/${category.imageUrl}'),
-                placeholder: AssetImage('assets/images/tesla.jpg'),
+class CategoryListItem extends StatelessWidget {
+  final Category category;
+  final Function onTap;
+
+  CategoryListItem({this.category, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: 120,
             ),
-          ),
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.black87.withOpacity(0.6),
+            Positioned.fill(
+                child: FadeInImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage('http://192.168.0.210:5200/${category.imageUrl}'),
+                  placeholder: AssetImage('assets/images/tesla.jpg'),
               ),
-            ), 
-          ),
-          Positioned.fill(
-            child: Center(
-              child: Text(
-                category.name,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+            ),
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black87.withOpacity(0.6),
+                ),
+              ), 
+            ),
+            Positioned.fill(
+              child: Center(
+                child: Text(
+                  category.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        elevation: 4,
       ),
-      elevation: 4,
     );
   }
 }
