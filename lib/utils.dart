63b36c1c9604d911bf42a2c8da0b0e7dc291a267
Leandro Souza <sl.leandro09@gmail.com>
2020-11-09 17:22:16 -0300

    Método para exibir um dialog de loading padrão adicionado em utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 
 const String apiBaseUrl = 'http://192.168.0.210:5200';
 
-/*void showLoadingDialog(BuildContext context, String text) {
+void showLoadingDialog(BuildContext context, String text) {
   showDialog(
     context: context,
-
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: Row(
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(width: 16.0),
+            Text(text),
+          ],
+        ),
+      );
+    }
   ); 
-}*/
+}
