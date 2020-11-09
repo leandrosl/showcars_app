b9243e953406229261fc0ecdbb6664fe44f12369
Leandro Souza <sl.leandro09@gmail.com>
2020-11-09 16:52:28 -0300

    método para buscar carros pela categoria adicionado na classe CarRepository

--- lib/repositories/car_repository.dart
+++ lib/repositories/car_repository.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
 
 import 'package:dio/dio.dart';
+
 import 'package:showcars_app/models/car.dart';
 
 class CarRepository {
   Future<List<Car>> getCarsByFactory(String factoryId) async {
     Response response = await Dio().get("http://192.168.0.210:5200/cars/$factoryId");
+    return response.data.map<Car>((car) => Car.fromJson(car)).toList();
+  }
+
+  Future<List<Car>> getCarsByCategory(String categoryId) async {
+    Response response = await Dio().get("http://192.168.0.210:5200/categories/cars/$categoryId");
     return response.data.map<Car>((car) => Car.fromJson(car)).toList();
   }
 }
