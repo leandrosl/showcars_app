47218bf189c19414bca9778eabcd353fcb591a85
Leandro Souza <sl.leandro09@gmail.com>
2020-11-11 16:32:02 -0300

    Adicionado novas propriedades a classe Car

--- lib/models/car.dart
+++ lib/models/car.dart
@@ -4,10 +4,40 @@ class Car {
   String id;
   Manufactor manufactor;
   String name;
+
   String horsepower;
+  String engineType;
+  String torque;
+  String gearbox;
+
+  String topSpeed;
+  String acceleration0To100;
+
+  String numberDoors;
+  String width;
+  String height;
+  String length;
+  String weight;
+
   String imageUrl;
 
-  Car({this.id, this.manufactor, this.name, this.horsepower, this.imageUrl});
+  Car({
+    this.id, 
+    this.manufactor, 
+    this.name, 
+    this.horsepower,
+    this.engineType,
+    this.torque,
+    this.gearbox,
+    this.topSpeed,
+    this.acceleration0To100,
+    this.numberDoors,
+    this.width,
+    this.height,
+    this.length, 
+    this.weight,
+    this.imageUrl,
+  });
 
   factory Car.fromJson(Map<String, dynamic> json) {
     return Car(
@@ -15,6 +45,16 @@ class Car {
       manufactor: Manufactor.fromJson(json['factory']),
       name: json['name'],
       horsepower: json['horsepower'],
+      engineType: json['engine_type'],
+      torque: json['torque'],
+      gearbox: json['gearbox'],
+      topSpeed: json['top_speed'],
+      acceleration0To100: json['acceleration_0to100'],
+      numberDoors: json['number_doors'],
+      width: json['width'],
+      height: json['height'],
+      length: json['length'],
+      weight: json['weight'],
       imageUrl: json['car_image'],
     );
   }
