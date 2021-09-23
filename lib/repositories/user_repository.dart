8407e5bd8fba0867833467ca7fe65ec95a0a6f8d
Leandro Souza <sl.leandro09@gmail.com>
2021-09-23 20:21:57 -0300

    Classe para gerenciar usuarios da API

---
+++ lib/repositories/user_repository.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:showcars_app/utils.dart';
+
+class UserRepository {
+  Future<dynamic> signIn(String email, String password) async {
+    try {
+      Map<String, dynamic> formData = {
+        "email": email,
+        "password": password
+      };
+
+      Response response = await Dio().post('$apiBaseUrl/auth/signin', data: formData);
+
+      if (response.statusCode == 200) {
+        return response.data;
+      }
+
+      throw Exception("Api fetch error");
+    } catch (err) {
+      throw Exception('$err');
+    }
+  }
+
+  Future<dynamic> signUp(String name, String email, String password) async {
+    Map<String, dynamic> formData = {
+      "name": name,
+      "email": email,
+      "password": password
+    };
+
+    Response response = await Dio().post('$apiBaseUrl/auth/signup', data: formData);
+
+    if (response.statusCode == 201) {
+      return await signIn(email, password);
+    }
+
+    throw Exception("API fetch error");
+  }
+}
